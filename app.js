ff4097ead9e2e7e12243405cdfb77b4b12637efb
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-09-14 20:55:10 +0530

    onclick changes added

---
+++ app.js
@@ -0,0 +1,8 @@
+ //----------------- Redirecting to the Create a account page ---------------------
+function LoginIn() {
+    window.location.href = "studentInfo.html";
+}
+
+function SingUp() {
+    window.location.href = "studentInfo.html";
+}
